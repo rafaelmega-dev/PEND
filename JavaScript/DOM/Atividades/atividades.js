4896655ba75e79e3b5e9ce0e75d828b58151e9ff
rafaelmega-dev <rafael.mega@aluno.senai.br>
2026-03-04 15:54:34 -0300

    opa

---
+++ JavaScript/DOM/Atividades/atividades.js
@@ -0,0 +1,47 @@
+// Atividade 1-Botão que deixa azul e vermelho
+
+let botaoAzul = document.querySelector("#botaoAzul");
+let botaoVermelho = document.querySelector("#botaoVermelho");
+let textoCor = document.querySelector("#textoCor");
+
+botaoAzul.addEventListener("click", function() {
+   textoCor.style.color = "blue";
+});
+
+botaoVermelho.addEventListener("click", function() {
+    textoCor.style.color = "red"; 
+});
+
+
+// Atividade 2-Botão que esconde o texto
+
+let botaoOcultar = document.querySelector("#botaoOcultar");
+let textoEscondido = document.querySelector("#textoEscondido");
+botaoOcultar.addEventListener("click", function() {
+    textoEscondido.classList.toggle("oculto");
+});
+
+
+// Atividade 3-Contador de caracteres
+
+let inputTexto = document.querySelector("#inputTexto");
+let paragContCarac = document.querySelector("#paragContCarac");
+
+inputTexto.addEventListener("keyup", function() {  
+    let contCarac = inputTexto.value.length;
+    paragContCarac.textContent = "Caracteres digitados: " + contCarac;
+});
+
+
+//Atividade 4-Contador de cliques
+
+let botaoContCliques = document.querySelector("#botaoContCliques");
+let paragContCliques = document.querySelector("#paragContCliques");
+let contCliques = 0;
+botaoContCliques.addEventListener("click", function() {
+    contCliques++;
+    paragContCliques.textContent = "Número de cliques: " + contCliques;
+});
+
+
+//
