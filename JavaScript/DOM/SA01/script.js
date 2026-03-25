536225b3cf40e17492aaed032d155d560497bcd4
rafaelmega-dev <rafael.mega@aluno.senai.br>
2026-03-25 15:58:19 -0300

    SA01: primeira versão do exercício

--- JavaScript/DOM/SA01/script.js
+++ JavaScript/DOM/SA01/script.js
@@ -1,5 +1,14 @@
-let botao = document.querySelector("#cadastrar");
-let mensagem = document.querySelector("#mensagem");
+//Regex para validar email
+let mascaraEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//Botão de cadastro
+let botao = document.getElementById("cadastrar");
+//Mensagem de cadastro
+let mensagem = document.getElementById("mensagem");
+//Inputs do formulário
+let nome = document.getElementById("nome");
+let telefone = document.getElementById("telefone");
+let email = document.getElementById("email");
+let senha = document.getElementById("senha");
 
 botao.addEventListener("click", function () {
     mensagem.textContent = "Cadastrado!";
